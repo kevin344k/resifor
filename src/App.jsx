528afa25945533c9df8ac0b97a65308b1ee056ed
kevin344k <kevin344k@gmail.com>
2025-08-10 07:16:12 -0500

    sdds

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import tailwindcss from '@tailwindcss/vite'
+import logo from "/logo.jpeg"
 import './App.css'
 
 function App() {
@@ -7,10 +7,13 @@ function App() {
 
   return (
     <>
-     
-      <p className="read-the-docs bg-red-500">
-        Click on the Vite and React logos to learn more
-      </p>
+<nav className="flex w-full px-12 items-center justify-between 
+  bg-white/30 backdrop-blur-md border border-white/20 rounded-xl shadow-lg">
+       
+  <img className="rounded-full size-12" src={logo} alt="logo" />
+  <p className="text-white font-medium">Pedir</p>
+</nav>
+
     </>
   )
 }
